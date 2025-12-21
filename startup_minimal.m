783b9c789e0e450f8d4a717e50bb5693679c4831
KangXu-MerilesLab <Kangxu90@gmail.com>
2025-12-21 11:32:17 -0500

    manage configure from site_ccny

--- startup_minimal.m
+++ startup_minimal.m
@@ -3,8 +3,15 @@ function startup_minimal()
 
 root = fileparts(mfilename('fullpath'));
 
+t = tic;
+
+
 % 1) Reset path to MATLAB default so you don't accidentally use old projects
 restoredefaultpath;
+
+dt_restore = toc(t);
+
+t = tic;
 
 % 2) Add only the folders you want (edit these names to match your project)
 mustAdd = {
@@ -32,6 +39,8 @@ for k = 1:numel(recursiveAdd)
         addpath(genpath(recursiveAdd{k}));
     end
 end
+dt_add = toc(t);
+
 
 % 4) Remove folders you never want on path (optional but recommended)
 %    e.g., huge .mat libraries, data, version-control metadata
@@ -45,17 +54,22 @@ removePatterns = {
 
 p = strsplit(path, pathsep);
 keep = true(size(p));
-% for i = 1:numel(p)
-%     for j = 1:numel(removePatterns)
-%         if contains(p{i}, removePatterns{j})
-%             keep(i) = false;
-%             break;
-%         end
-%     end
-% end
+for i = 1:numel(p)
+    for j = 1:numel(removePatterns)
+        if contains(p{i}, removePatterns{j})
+            keep(i) = false;
+            break;
+        end
+    end
+end
 path(strjoin(p(keep), pathsep));
 
-rehash toolboxcache;
+t = tic;
+
+rehash;
+
+dt_rehash = toc(t);
+
 
 fprintf('[startup_minimal] Path set for project: %s\n', root);
 end
