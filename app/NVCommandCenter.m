fe10bcafd2e06ad006bf067d2a0555c6a162fec4
KangXu-MerilesLab <Kangxu90@gmail.com>
2026-05-27 10:44:08 -0400

    pulse-f configure change

--- app/NVCommandCenter.m
+++ app/NVCommandCenter.m
@@ -477,9 +477,9 @@ switch Mode
 
     case 'Pulsed/f-sweep'
 
-          ConfigVoltageForRange = true;
+          % ConfigVoltageForRange = true;
 
-          % ConfigVoltageForRange = false;
+           ConfigVoltageForRange = false;
         % general config
         %default samplerate for pulse-ODMR
         if  ConfigVoltageForRange 
